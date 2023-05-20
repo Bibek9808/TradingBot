6762a8af19c895190b0363b2df20e08dc7ee6d74
Bibek Shrestha <bibekshrestha@innovatetech.co>
2023-05-20 19:32:57 +0545

    Data rates for 15 mins and hourly candle added

--- Pages/navigate.py
+++ Pages/navigate.py
@@ -10,16 +10,27 @@ def SetTimeZone():
  # Define the timezone for UTC-4 (Eastern Daylight Time)
 
 # Get the current time in UTC-4
-    utc_from = datetime(2023, 5, 10, tzinfo=timezone)
-    utc_to = datetime(2023, 5, 19, tzinfo=timezone)
+    utc_from = datetime(2022, 1, 1, tzinfo=timezone)
+    utc_to = datetime(2023, 1, 1, tzinfo=timezone)
     GetRates(utc_from, utc_to)
 
 def GetRates(dateFrom,utcTo):
-    rates = mt5.copy_rates_range("GBPJPY", mt5.TIMEFRAME_D1, dateFrom, utcTo) 
+
+    #data rates of 15 minutes candle 
+    # quarter_mins_rates_frame = 
+    #data rates of hourly candle
+    hourly_rates = mt5.copy_rates_range("GBPJPY", mt5.TIMEFRAME_H1, dateFrom, utcTo)
     # create DataFrame out of the obtained data
-    rates_frame = pd.DataFrame(rates)
+    hourly_rates_frame = pd.DataFrame(hourly_rates)
    # convert time in seconds into the datetime format
-    rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')     
-    # display data
-    print("\nDisplay dataframe with data")
-    print(rates_frame.head(24))      
+    hourly_rates_frame['time']=pd.to_datetime(hourly_rates_frame['time'], unit='s')     
+    print("\nDisplay dataframe with data for hourly candle")
+    print(hourly_rates_frame.head(24))
+
+    #data rates of 15 minutes candle
+    quarter_mins_rates = mt5.copy_rates_range("GBPJPY", mt5.TIMEFRAME_M15, dateFrom, utcTo)
+    quarter_mins_rates = pd.DataFrame(quarter_mins_rates)
+    quarter_mins_rates['time']=pd.to_datetime(quarter_mins_rates['time'], unit='s')     
+    print("\nDisplay dataframe with data for 15 minutes candle")
+    print(quarter_mins_rates.head(24))
+
