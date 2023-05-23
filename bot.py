4936e678d17f04765c12011118cba408dc6728b4
Bibek Shrestha <bibekshrestha@innovatetech.co>
2023-05-23 19:08:49 +0545

    code refactor for Entering name and password

--- bot.py
+++ bot.py
@@ -2,11 +2,4 @@ from Constants import messages
 from authenticate import connect_to_metatrader
 
 print(messages.welcome_message)
-
-login = input(messages.yesNoDialogueBox)
-if(login == "Yes"):
-    userName = input(messages.enterLoginName)
-    password = input(messages.enterPassword)
-    connect_to_metatrader()
-else:
-    print(messages.byeMessage)
+connect_to_metatrader()
